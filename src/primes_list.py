16e49d8bf4e686bf270416d55eb4a48c3501025e
BaikNadia <baikowa.nadia@gmail.com>
2025-04-13 18:32:39 +0300

    cang primes

---
+++ src/primes_list.py
@@ -0,0 +1,12 @@
+def primes():
+    n = 2
+    primes_list = []
+    while True:
+        if all(n % p != 0 for p in primes_list):
+            yield n
+            primes_list.append(n)
+        n += 1
+
+p = primes()
+for i in range(10):
+    print(next(p))
